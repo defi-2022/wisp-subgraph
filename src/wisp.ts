7c63f99e8c41425428485eaacdbf31cf3a1969f6
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-07-07 08:53:45 +0200

    Add initial payment subgraph implementation

--- src/wisp.ts
+++ src/wisp.ts
@@ -1,56 +1,13 @@
-import { BigInt } from "@graphprotocol/graph-ts"
-import { Wisp, Payment } from "../generated/Wisp/Wisp"
-import { ExampleEntity } from "../generated/schema"
+import { Payment } from "../generated/Wisp/Wisp"
+import { Payment as PaymentEntity } from "../generated/schema"
 
 export function handlePayment(event: Payment): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from.toHex())
+  const entity = new PaymentEntity(event.params.index.toHex());
 
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from.toHex())
+  entity.publicKey = event.params.publicKey.toHexString();
+  entity.commitment = event.params.commitment;
+  entity.encryptedData = event.params.encryptedData;
+  entity.sender = event.transaction.from.toHexString()
 
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
-  }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.publicKey = event.params.publicKey
-  entity.commitment = event.params.commitment
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.FIELD_SIZE(...)
-  // - contract.ROOT_HISTORY_SIZE(...)
-  // - contract.depositVerifier(...)
-  // - contract.hasher(...)
-  // - contract.index(...)
-  // - contract.isValidRoot(...)
-  // - contract.levelHashes(...)
-  // - contract.levels(...)
-  // - contract.maxSize(...)
-  // - contract.roots(...)
-  // - contract.tokens(...)
-  // - contract.zeros(...)
+  entity.save();
 }
