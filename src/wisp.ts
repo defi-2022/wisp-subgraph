b117f563859d8f6ccb7ae9a2e2f14e2fac020d06
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-07-10 16:48:07 +0200

    Extend subgraph data

--- src/wisp.ts
+++ src/wisp.ts
@@ -2,12 +2,14 @@ import { Payment } from "../generated/Wisp/Wisp"
 import { Payment as PaymentEntity } from "../generated/schema"
 
 export function handlePayment(event: Payment): void {
-  const entity = new PaymentEntity(event.params.index.toHex());
+  const entity = new PaymentEntity(event.params.index.toString());
 
-  entity.publicKey = event.params.publicKey.toHexString();
+  entity.publicKey = "0x" + event.params.publicKey.toHexString().slice(2).padStart(64, "0");
   entity.commitment = event.params.commitment;
   entity.encryptedData = event.params.encryptedData;
-  entity.sender = event.transaction.from.toHexString()
+  entity.sender = event.transaction.from;
+  entity.txHash = event.transaction.hash;
+  entity.blockTimestamp = event.block.timestamp;
 
   entity.save();
 }
