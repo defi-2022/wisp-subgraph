e334c21be857a48401f5ffebb93e4bfe593c3188
Uladzislau Radkevich <uladzislau.radkevich@gmail.com>
2022-08-01 21:26:09 +0200

    Add numeric index field to Payment entity

--- src/wisp.ts
+++ src/wisp.ts
@@ -4,6 +4,7 @@ import { Payment as PaymentEntity } from "../generated/schema"
 export function handlePayment(event: Payment): void {
   const entity = new PaymentEntity(event.params.index.toString());
 
+  entity.index = event.params.index;
   entity.publicKey = "0x" + event.params.publicKey.toHexString().slice(2).padStart(64, "0");
   entity.commitment = event.params.commitment;
   entity.encryptedData = event.params.encryptedData;
